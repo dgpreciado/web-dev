49529c61f3a31b3192d79a8ed2a7173b3ff01118
dgpreciado <37858944+dgpreciado@users.noreply.github.com>
2019-12-21 15:35:58 -0700

    loops

---
+++ ch11JavaScript_ControlFlow/sec132Loops/loops.js
@@ -0,0 +1,15 @@
+var count = 1;
+
+while(count < 6 ){
+    console.log("count is: "+ count);
+    count++;
+}
+
+//next loop
+var str = "hello";
+
+var count = 0;
+while(count < str.length){
+    console.log(str[count]);
+    count++;
+}
